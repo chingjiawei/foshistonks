98c545ff4af619ad4535627d632a1e91448a793c
MaryCao <150237M@mymail.nyp.edu.sg>
2020-03-31 00:29:02 +0800

    Index.php to login (create php session)
    UI + call login service

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>FoshiStonk</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="images/icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="images/icons/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Bootstrap libraries -->
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
+
+</head>
+
+<!-- Page passes the container for the graph to the program -->
+
+<body id="index_body">
+    <div class='col-2 login_left'>
+        <img src="src/img/logo.png" alt="">
+        <p><i>one-stop platform</i></p>
+        <p class="login_descript">THE ONLY PLACE TO LEARN ABOUT STONKS, EARN SOME STONKS, INVEST IN STONKS,
+HAPPY STONKING!</p>
+    </div>
+    <div class='col-2 login_right'>
+        <img src="src/img/yoshi_house.png" alt="">
+
+        <form id="login_form">
+            <input type="text" class="form-control" id="username" placeholder="Username"value="">
+            <input type="password" class="form-control" id="password" placeholder="Password" value="">
+            <div class="button_row">    
+                <button type="submit" class="btn_login">Log In</button>
+                <span> | </span> 
+                <a class="a_signup" ref="signup.php">Sign Up</a>
+            </div>
+        </form>
+
+        <label id="error" class="text-danger"></label>
+        
+    </div>
+
+    <!-- <img id="bg_overlay" src="src/bg_check/login.png" alt=""> -->
+
+    <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="foshistonks_bot"
+            data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+	<script>
+        function showError(message) {
+            // Display an error under the the predefined label with error as the id
+            $('#error').text(message);
+        }
+
+        $("#login_form").submit(async (event) => {
+            //Prevents screen from refreshing when submitting
+            event.preventDefault();
+
+            var serviceURL = "http://localhost:5013/login/";
+            var homeURL = "http://localhost/home.php";
+
+            //Get form data 
+            var username = $('#username').val();
+            var password = $('#password').val();
+            // var availability = parseInt($("#availability").val());
+
+            // form the POST url which includes the dynamic username
+            serviceURL += username;
+            try {
+                const response =
+                    await fetch(
+                        serviceURL, {
+                        method: 'POST',
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ message: message })
+                    });
+                const data = await response.json();
+
+                if (response.ok) {
+                    // create php session first
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.open("GET", "session_maker.php?username="+username, true);
+
+                    xmlhttp.onreadystatechange = function(){
+                        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+                            alert("Done! Session created.");
+                        }
+                    };
+                    // relocate to home page
+                    window.location.replace(homeURL);
+                    return false;
+                } else {
+                    console.log(data);
+                    showError(data.message);
+                }
+            } catch (error) {
+                // Errors when calling the service; such as network error, 
+                // service offline, etc
+                showError
+                    ("There is a problem logging into this account, please try again later. " + error);
+
+            } // error
+        });
+        function onTelegramAuth(user) {
+                alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+            }
+            
+        </script>
+    </script>
+</body>
+
+</html>
