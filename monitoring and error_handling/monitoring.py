da842676ef8c6b08afdb0cd825b224020fd29e75
cxlim96 <54776545+cxlim96@users.noreply.github.com>
2020-03-31 14:14:08 +0800

    Monitoring and Error_handling microservices
    use the test[name].py file to try the stuff. The function codes in test[]name].py should be inside composite microservices that is calling [name] automic microservices

---
+++ monitoring and error_handling/monitoring.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# The above shebang (#!) operator tells Unix-like environments
+# to run this file as a python3 script
+import json
+import sys
+import os
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from datetime import datetime
+import json
+import pika
+import telegram
+import requests
+import os
+# Communication patterns:
+# Use a message-broker with 'direct' exchange to enable interaction
+#import pika
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/FoshiStonks'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+CORS(app)
+
+class MonitoringLog(db.Model):
+    __tablename__ = 'monitoring'
+ 
+    log_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(10), nullable=False)
+    log_content = db.Column(db.String(500), nullable=False)
+    log_from = db.Column(db.String(20), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    
+    def __init__(self, log_id, user_id,log_content,log_from,timestamp):
+        self.log_id = log_id
+        self.user_id = user_id
+        self.log_content = log_content
+        self.log_from = log_from
+        self.timestamp = timestamp 
+
+
+    # def json(self):
+    #     return{'notification_id': self.notification_id,'project_id': self.project_id,'user_id': self.user_id,'message': self.message,'phone_number': self.phone_number,'timestamp': self.timestamp}
+#db.create_all() 
+
+def receiveLog():
+    hostname = "localhost" # default host
+    port = 5672 # default port
+    # connect to the broker and set up a communication channel in the connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+    channel = connection.channel()
+
+    # set up the exchange if the exchange doesn't exist
+    exchangename="monitoring_direct"
+    channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
+    # prepare a queue for receiving messages
+    channelqueue = channel.queue_declare(queue='', exclusive=True) # '' indicates a random unique queue name; 'exclusive' indicates the queue is used only by this receiver and will be deleted if the receiver disconnects.
+        # If no need durability of the messages, no need durable queues, and can use such temp random queues.
+    queue_name = channelqueue.method.queue
+    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='handle.monitoring') # bind the queue to the exchange via the key
+        # Can bind the same queue to the same exchange via different keys
+
+    # set up a consumer and start to wait for coming messages
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming() # an implicit loop waiting to receive messages; it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    print("Received an log by " + __file__)
+    processLog(json.loads(body))
+    print() # print a new line feed
+
+def processLog(log):
+    print("Recording an incoming log:")
+    print("Storing Log to DB:")
+    #Inserting record one by one
+    monitoringLog = MonitoringLog(None, log["user_id"], log["log_content"], log["log_from"],log["timestamp"]) 
+    db.session.add(monitoringLog) 
+    db.session.commit()
+    print("Data stored in DB successfully")
+    print("this is a log from " + log["log_from"])
+    print(log)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
+if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')
+    print("This is " + os.path.basename(__file__) + ": monitoring everything...")
+    receiveLog()
---
+++ monitoring and error_handling/testError.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+# The above shebang (#!) operator tells Unix-like environments
+# to run this file as a python3 script
+
+import json
+import sys
+import os
+import random
+import datetime
+import pika 
+import uuid
+import csv
+
+
+# Communication patterns:
+# Use HTTP calls to enable interaction with shipping
+# Use language-level function calls to enable interaction with monitoring and error_handling
+import requests
+errorURL = "http://127.0.0.1:5002/error"
+
+#class CreateError:
+    # Load existing orders from a JSON file (for simplificity here). In reality, orders will be stored in DB. 
+    # with open('projectDetails.json') as noti_json_file:
+    #     noti = json.load(noti_json_file)
+    # noti_json_file.close()
+
+    # Find the max of all existing "order_id" to be used as the last order_id; if in actual DB, the uniqueness of "order_id" will be managed by DBMS
+    #last_noti_id = max([ o["notification_id"] for o in noti["noti"] ])
+
+# def orders_json():
+#     """return all orders as a JSON object (not a string)"""
+#     return Order.orders
+    
+# def orders_save(orders_file):
+#     """ save all orders to a file"""
+#     with open(orders_file, 'w') as order_json_outfile:
+#         json.dump(Order.orders, order_json_outfile, indent=2, default=str) # convert a JSON object to a string
+#     order_json_outfile.close()
+
+# class Order_Item:
+#     def __init__(self):
+#         self.item_id = 0
+#         self.book_id = "0123456789abc"
+#         self.quantity = 0
+#         self.order_id = 0
+
+#     # return an order item as a JSON object
+#     def json(self):
+#         return {'item_id': self.item_id, 'book_id': self.book_id, 'quantity': self.quantity, 'order_id': self.order_id}
+
+# def get_all():
+#     """Return all orders as a JSON object"""
+#     return Order.orders
+ 
+# def find_by_order_id(order_id):
+#     """Return an order (orders) of the order_id"""
+#     order = [ o for o in Order.orders["orders"] if o["order_id"]==order_id ]
+#     if len(order)==1:
+#         return order[0]
+#     elif len(order)>1:
+#         return {'message': 'Multiple orders found for id ' + str(order_id), 'orders': order}
+#     else:
+#         return {'message': 'Order not found for id ' + str(order_id)}
+ 
+def create_error():
+    # assume status==200 indicates success
+    status = 200
+    message = "Success"
+
+    # Load the order info from a cart (from a file in this case; can use DB too, or receive from HTTP requests)
+    # try:
+    #     with open(noti_input) as sample_noti_file:
+    #         cart_order = json.load(sample_noti_file)
+    # except:
+    #     status = 501
+    #     message = "An error occurred in loading the noti file."
+    # finally:
+    #     sample_order_file.close()
+    # if status!=200:
+    #     print("Failed noti creation.")
+    #     return {'status': status, 'message': message}
+
+    # Create a new order: set up data fields in the order as a JSON object (i.e., a python dictionary)
+    
+    
+    errorFrom = "sendingNoti"
+    email = "testing@email.com"
+    userid = "testing123"
+    
+       
+    errorJson = ({        "errorFrom": errorFrom,
+                         "timestamp": datetime.datetime.now(),
+                         "user_id":userid,
+                         "email": email
+        })
+    # check if order creation is successful
+    if len(errorJson) <1:
+        status = 404
+        message = "Empty error."
+    # Simulate other errors in order creation via a random bit
+    # result = bool(random.getrandbits(1))
+    # if not result:
+    #     status = 500
+    #     message = "A simulated error occurred when creating the noti."
+
+    if status!=200:
+        print("Failed error creation.")
+        return {'status': status, 'message': message}
+
+    # Append the newly created order to the existing orders
+    #Order.orders["orders"].append(order)
+    # Increment the last_order_id; if using a DB, DBMS can manage this
+    #Order.last_order_id = Order.last_order_id + 1
+    # Write the newly created order back to the file for permanent storage; if using a DB, this will be done by the DBMS
+    #orders_save("orders.new.json")
+
+    # Return the newly created order when creation is succssful
+    if status==200:
+        print("OK error creation.")
+        return errorJson
+
+def send_error(error):
+    #"""inform Shipping/Monitoring/Error as needed"""
+    # default username / password to the borker are both 'guest'
+    hostname = "localhost" # default broker hostname. Web management interface default at http://localhost:15672
+    port = 5672 # default messaging port.
+    # connect to the broker and set up a communication channel in the connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+        # Note: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
+        # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
+    channel = connection.channel()
+
+    # set up the exchange if the exchange doesn't exist
+    exchangename="error_direct"
+    channel.exchange_declare(exchange=exchangename, exchange_type='direct')
+
+    # prepare the message body content
+    message = json.dumps(error, default=str) # convert a JSON object to a string
+
+    # send the message
+    # always inform Monitoring for logging no matter if successful or not
+    channel.basic_publish(exchange=exchangename, routing_key="handle.error", body=message)
+        # By default, the message is "transient" within the broker;
+        #  i.e., if the monitoring is offline or the broker cannot match the routing key for the message, the message is lost.
+        # If need durability of a message, need to declare the queue in the sender (see sample code below).
+
+    # if "status" in noti: # if some error happened in order creation
+    #     # inform Error handler
+    #     channel.queue_declare(queue='errorhandler', durable=True) # make sure the queue used by the error handler exist and durable
+    #     channel.queue_bind(exchange=exchangename, queue='errorhandler', routing_key='shipping.error') # make sure the queue is bound to the exchange
+    #     channel.basic_publish(exchange=exchangename, routing_key="shipping.error", body=message,
+    #         properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
+    #     )
+    #     print("Order status ({:d}) sent to error handler.".format(order["status"]))
+    # else: # inform Shipping and exit, leaving it to order_reply to handle replies
+    #     # Prepare the correlation id and reply_to queue and do some record keeping
+    #     corrid = str(uuid.uuid4())
+    #     row = {"order_id": order["order_id"], "correlation_id": corrid}
+    #     csvheaders = ["order_id", "correlation_id", "status"]
+    #     with open("corrids.csv", "a+", newline='') as corrid_file: # 'with' statement in python auto-closes the file when the block of code finishes, even if some exception happens in the middle
+    #         csvwriter = csv.DictWriter(corrid_file, csvheaders)
+    #         csvwriter.writerow(row)
+    #     replyqueuename = "shipping.reply"
+    #     # prepare the channel and send a message to Shipping
+    #     channel.queue_declare(queue='shipping', durable=True) # make sure the queue used by Shipping exist and durable
+    #     channel.queue_bind(exchange=exchangename, queue='shipping', routing_key='shipping.order') # make sure the queue is bound to the exchange
+    #     channel.basic_publish(exchange=exchangename, routing_key="shipping.order", body=message,
+    #         properties=pika.BasicProperties(delivery_mode = 2, # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange, which are ensured by the previous two api calls)
+    #             reply_to=replyqueuename, # set the reply queue which will be used as the routing key for reply messages
+    #             correlation_id=corrid # set the correlation id for easier matching of replies
+    #         )
+    #     )
+    #     print("Order sent to shipping.")
+    print("error sent")
+    # close the connection to the broker
+    connection.close()
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is " + os.path.basename(__file__) + ": creating an error...")
+    error = create_error()
+    send_error(error)
+#    print(get_all())
+#    print(find_by_order_id(3))
