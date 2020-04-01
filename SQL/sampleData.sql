c79ce6cdf3b46a61e385d91f45b6f659acb0d67d
Xiaoyue Zhang <xyzhang.2018@sis.smu.edu.sg>
2020-04-02 02:15:45 +0800

    Updated sample data

--- SQL/sampleData.sql
+++ SQL/sampleData.sql
@@ -6,6 +6,7 @@
 INSERT INTO `account` (`username`, `password`, `email`, `phoneNumber`, `telegramid`, `stonks`, `equipHead`, `equipBody`, `equipHand`, `equipPet`) VALUES 
 ('Yoshi', 'youshi123', 'youshi@gmail.com', '67009000', '12345', 100.00, 1, Null, Null, Null), 
 ('James', 'james123', 'james@gmail.com', '98882345', '98572', 100.00, Null, Null, Null, 3), 
+('mary', '123', 'mary@gmail.com', '74653725', '98972', 100.00, Null, Null, Null, 3), 
 ('Amy', 'amy123', 'amy@gmail.com', '66667888', '23456', 100.00, Null, 2, Null, Null);
 
 ---
@@ -13,11 +14,10 @@ INSERT INTO `account` (`username`, `password`, `email`, `phoneNumber`, `telegram
 ---
 
 INSERT INTO `accessory` (`accessoryID`, `accessoryName`, `accessoryDesc`, `category`, `src`) VALUES
-(1, 'Santa Hat', 'Ho ho ho! It\'s Christmas Time!', 'equipHead', './src/img/1.png'),
-(2, 'Spidersuit', 'A Spidersuit specially created for Yoshi', 'equipBody', './src/img/2.png'),
-(3, 'Jake The Dog', 'Jake the Dog from Adventure Time. Get one today!', 'equipPet', './src/img/3.png'),
-(4, 'Ice Cream', 'I know you are thirsty. Get one for yourself!', 'equipHand', './src/img/4.png'),
-(5, 'Crown', 'Be your own king!', 'equipHead', './src/img/5.png');
+(1, 'Santa Hat', 'Ho ho ho! It\'s Christmas Time!', 'equipHead', 'hat2.png'),
+(2, 'Spidersuit', 'A Spidersuit specially created for Yoshi', 'equipBody', 'body3.png'),
+(3, 'Jake The Dog', 'Jake the Dog from Adventure Time. Get one today!', 'equipPet', 'pet2.png'),
+(4, 'Ice Cream', 'Eat me~', 'equipHand', 'hand1.png');
 
 ---
 --- Dumping data for table `stock`
@@ -36,6 +36,7 @@ INSERT INTO `stock` (`stockname`, `spoofname`) VALUES
 INSERT INTO `inventory` (`username`, `accessoryID`, `quantity`) VALUES
 ('Yoshi', 1, 1),
 ('James', 3, 1),
+('mary', 3, 1),
 ('Amy', 2, 1);
 
 ---
